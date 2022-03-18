a8160a49fcdf15798ccfa80d85dbb4034851c3be
Guillaume <guill.lambert@orange.fr>
2022-03-18 17:23:49 +0100

    Island Editor pop Tetris

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject current;
+    private string currentType;
+    
+    public void PopBuild(string key)
+    {
+        Debug.Log(key);
+        if(current != null) Pooler.instance.Depop(currentType,current);
+        currentType = key;
+        current = Pooler.instance.Pop(currentType);
+        current.transform.position = Vector3.up;
+    }
+}
--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -7,45 +7,32 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-    [SerializeField] private Material initMat;
-    [SerializeField] private Material cantPlaceMat;
-    [SerializeField] private Material canPlaceMat;
     [SerializeField] private Block[] blocks;
 
-    private void Update()
+    private void OnEnable()
     {
-        
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Ray ray = Camera.main.ScreenPointToRay(touch.position);
-            
-            if(Physics.Raycast(ray,out RaycastHit hit))
-            {
-                float posX  = Mathf.Round(hit.point.x);
-                float posZ = Mathf.Round(hit.point.z);
-                transform.position = new Vector3(posX, 0, posZ);
-            }
-            IsPlaceable();
-            if (touch.phase == TouchPhase.Ended)
-            {
-                if (IsPlaceable() == 2)
-                {
-                    foreach (var block in blocks)
-                        GameManager.instance.blocks.Add(block.transform.position,block);
-                    foreach (var block in blocks)
-                        block.AddToAdjacents();
-                    enabled = false;
-                }
-                else
-                {
-                    Pooler.instance.Depop("Tetris",gameObject);
-                }
-            }
+        Debug.Log("enable");
+        StartCoroutine(WaitForRelease());
+    }
 
-           
+    private IEnumerator WaitForRelease()
+    {
+        while (Input.touchCount == 0) yield return null;
+        while (gameObject.activeSelf)
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                pos.z = 0;
+                transform.position = pos;
+                yield return null;
+            }
+            yield return null;
         }
     }
+    
+    
 
     private int state = 0;
     int IsPlaceable()
