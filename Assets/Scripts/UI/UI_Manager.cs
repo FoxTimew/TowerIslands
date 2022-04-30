44a240c104f0cc5a4b11414026c32d1e4cbf46c2
Alexandre Daspe <alexandre.daspe@gmail.com>
2022-04-30 22:52:50 +0200

    Setting Up UI

---
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField] private RectTransform 
+        mainMenu,
+        creditsMenu,
+        settingsMenu,
+        islandMenu,
+        islandEditorMenu,
+        levelSelectionMenu,
+        playingLevelMenu,
+        levelPreparationMenu,
+        feedbackUI,
+        defeatMenu,
+        victoryMenu;
+
+    public void OpenMainMenu()
+    {
+        
+    }
+
+    public void CloseMainMenu()
+    {
+        
+    }
+    public void OpenCreditsMenu()
+    {
+        
+    }
+
+    public void CloseCreditsMenu()
+    {
+        
+    }
+    public void OpenSettingsMenu()
+    {
+        
+    }
+
+    public void CloseSettingsMenu()
+    {
+        
+    }
+    public void OpenIslandMenu()
+    {
+        
+    }
+
+    public void CloseIslandMenu()
+    {
+        
+    }
+    public void OpenIslandEditorMenu()
+    {
+        
+    }
+
+    public void CloseIslandEditorMenu()
+    {
+        
+    }
+    public void OpenLevelSelectionMenu()
+    {
+        
+    }
+
+    public void CloseLevelSelectionMenu()
+    {
+        
+    }
+    public void OpenFeedbackUIMenu()
+    {
+        
+    }
+
+    public void CloseFeedbackUIMenu()
+    {
+        
+    }
+    public void OpenPlayLevelMenu()
+    {
+        
+    }
+
+    public void ClosePlayLevelMenu()
+    {
+        
+    }
+    public void OpenDefeatMenu()
+    {
+        
+    }
+
+    public void CloseDefeatMenu()
+    {
+        
+    }
+    public void OpenVictoryMenu()
+    {
+        
+    }
+
+    public void CloseVictoryMenu()
+    {
+        
+    }
+    public void OpenLevelPreparationMenu()
+    {
+        
+    }
+
+    public void CloseLevelPreparationMenu()
+    {
+        
+    }
+}
