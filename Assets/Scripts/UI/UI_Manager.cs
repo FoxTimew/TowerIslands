ccdebf924aac9821d0f3c37fc8329bc75e3c4902
ADaspe <alexandre.daspe@gmail.com>
2022-05-03 17:31:04 +0200

    Cloud Transition WIP

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -4,11 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+public enum MenuEnum
+{
+    MainMenu = 1,
+    CreditsMenu = 2,
+    SettingsMenu = 3,
+    IslandMenu = 4,
+    IslandEditorMenu = 5,
+    LevelSelectionMenu = 6,
+    LevelPreparationMenu = 7,
+    PlayingLevelMenu = 8,
+    FeedbackUI = 9,
+    TowerInfoUI = 10,
+    DefeatMenu =11,
+    VictoryMenu = 12
+}
+
 public class UI_Manager : MonoBehaviour
 {
+    
     public static UI_Manager instance;
+
+    [Header("Menu References")] [SerializeField]
+    private RectTransform  mainMenu;
     [SerializeField] private RectTransform 
-        mainMenu,
         creditsMenu,
         settingsMenu,
         islandMenu,
@@ -17,9 +36,15 @@ public class UI_Manager : MonoBehaviour
         playingLevelMenu,
         levelPreparationMenu,
         feedbackUI,
+        towerInfoUI,
         defeatMenu,
         victoryMenu;
 
+    [Header("Transition Reference")]
+    [SerializeField] private Transform transitionClouds;
+    [SerializeField] private float transitionDuration;
+    private Vector3 initialCloudPosition;
+    
     private void Awake()
     {
         if (instance == null)
@@ -28,103 +53,83 @@ public class UI_Manager : MonoBehaviour
         }
     }
 
-    public void OpenMainMenu()
+    private void Start()
+    {
+        initialCloudPosition = transitionClouds.position;
+    }
+
+    public void OpenMenu(int menuEnumValue)
+    {
+        switch ((MenuEnum)menuEnumValue)
+        {
+            case (MenuEnum.MainMenu):
+                break;
+            case (MenuEnum.CreditsMenu):
+                break;
+            case (MenuEnum.SettingsMenu):
+                break;
+            case (MenuEnum.IslandMenu):
+                break;
+            case (MenuEnum.IslandEditorMenu):
+                break;
+            case (MenuEnum.LevelSelectionMenu):
+                break;
+            case (MenuEnum.LevelPreparationMenu):
+                break;
+            case (MenuEnum.PlayingLevelMenu):
+                break;
+            case (MenuEnum.FeedbackUI):
+                break;
+            case (MenuEnum.TowerInfoUI):
+                break;
+            case (MenuEnum.DefeatMenu):
+                break;
+            case (MenuEnum.VictoryMenu):
+                break;
+        }
+    }
+
+    public void CloseMenu(int menuEnumValue)
+    {
+        switch ((MenuEnum)menuEnumValue)
+        {
+            case (MenuEnum.MainMenu):
+                break;
+            case (MenuEnum.CreditsMenu):
+                break;
+            case (MenuEnum.SettingsMenu):
+                break;
+            case (MenuEnum.IslandMenu):
+                break;
+            case (MenuEnum.IslandEditorMenu):
+                break;
+            case (MenuEnum.LevelSelectionMenu):
+                break;
+            case (MenuEnum.LevelPreparationMenu):
+                break;
+            case (MenuEnum.PlayingLevelMenu):
+                break;
+            case (MenuEnum.FeedbackUI):
+                break;
+            case (MenuEnum.TowerInfoUI):
+                break;
+            case (MenuEnum.DefeatMenu):
+                break;
+            case (MenuEnum.VictoryMenu):
+                break;
+        }
+    }
+
+    public void TransitionMenu(int menuIDToClose, int menuIDToOpen)
     {
         
     }
 
-    public void CloseMainMenu()
+    public void CloudTransition()
     {
-        
-    }
-    public void OpenCreditsMenu()
-    {
-        
-    }
-
-    public void CloseCreditsMenu()
-    {
-        
-    }
-    public void OpenSettingsMenu()
-    {
-        
-    }
-
-    public void CloseSettingsMenu()
-    {
-        
-    }
-    public void OpenIslandMenu()
-    {
-        
-    }
-
-    public void CloseIslandMenu()
-    {
-        
-    }
-    public void OpenIslandEditorMenu()
-    {
-        
-    }
-
-    public void CloseIslandEditorMenu()
-    {
-        
-    }
-    public void OpenLevelSelectionMenu()
-    {
-        
-    }
-
-    public void CloseLevelSelectionMenu()
-    {
-        
-    }
-    public void OpenFeedbackUIMenu()
-    {
-        
-    }
-
-    public void CloseFeedbackUIMenu()
-    {
-        
-    }
-    public void OpenPlayLevelMenu()
-    {
-        
-    }
-
-    public void ClosePlayLevelMenu()
-    {
-        
-    }
-    public void OpenDefeatMenu()
-    {
-        
-    }
-
-    public void CloseDefeatMenu()
-    {
-        
-    }
-    public void OpenVictoryMenu()
-    {
-        
-    }
-
-    public void CloseVictoryMenu()
-    {
-        
-    }
-    public void OpenLevelPreparationMenu()
-    {
-        
-    }
-
-    public void CloseLevelPreparationMenu()
-    {
+        transitionClouds.DOMove(Vector3.zero, 
+                transitionDuration).OnComplete(() => { transitionClouds.position = -transitionClouds.position;
+            });
         
     }
 }
